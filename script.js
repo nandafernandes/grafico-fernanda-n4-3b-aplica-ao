3919edad5f39dbe153d447e3f5b7b11a71d8a09c
Nanda <102810392+nandafernandes@users.noreply.github.com>
2024-10-23 08:40:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const ctx = document.getElementById("myChart").getContext("2d");
+const myChart = new Chart (ctx,{
+    type: "pie",
+    date:  {
+        labels:[
+            "Instagram",
+             "WhatsApp",
+             "Facebook",
+             "WeChat",
+             "TikTok",
+             "DouYin",
+             "Twitter",
+             "Telegram",
+            "FB Messenger",
+             "LINE"
+        ]
+    }
+
+
+
+
+}
+     
+    )
